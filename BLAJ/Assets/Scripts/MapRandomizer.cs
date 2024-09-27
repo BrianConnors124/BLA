6c8ba3cebdc3b33f7930eb3aad168fe8b2fb3faf
BrianConnors124 <bmconno000@birdvilleschools.net>
2024-09-26 21:27:07 -0500

    SaveState

--- BLAJ/Assets/Scripts/MapRandomizer.cs
+++ BLAJ/Assets/Scripts/MapRandomizer.cs
@@ -1,28 +1,38 @@
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MapRandomizer : MonoBehaviour
 {
-    public GameObject[] room;
-    [SerializeField] private int rand;
-    
-    // Start is called before the first frame update
-    void Start()
+    [Header("Location And Direction")] 
+    [SerializeField] private Vector3 _location;
+    [SerializeField] private Vector2 _direction;
+
+    [Header("GameObject Inst")] 
+    [SerializeField] private GameObject[] platform;
+    public bool placeable;
+
+
+    private void Awake()
     {
-        rand = Random.Range(0, room.Length);
-        InstantiateLevel();
+        _location = transform.position;
+        StartCoroutine(TryPlace());
     }
 
-    private void InstantiateLevel()
+    private IEnumerator TryPlace()
     {
-        for (var i = 1; i <= 4; i++)
+
+        for (var i = 0; i < platform.Length; i++)
         {
-            var mod = 4 / i;
-            
-            Instantiate(room[rand], new Vector2(transform.position.x + mod * 2, transform.position.y + mod * 2), Quaternion.identity);
-            
-            rand = Random.Range(0, room.Length);
+            var rand = Random.Range(0, 1);
+            _direction = new Vector2(Random.Range(-1, 1), Random.Range(-1, 1));
+            BoxCollider2D platformCol = platform[i].GetComponent<BoxCollider2D>();
+            Vector2 colSize = platformCol.size;
+            Vector2 scaleSize = platform[i].transform.localScale;
+            Instantiate(platform[i], new Vector2((float) _location.x + _direction.x * colSize.x * scaleSize.x, (float) _location.y + _direction.y * colSize.y * scaleSize.y), Quaternion.identity);
+            Debug.Log(placeable);
+            yield return new WaitForSeconds(1);
         }
     }
 }
