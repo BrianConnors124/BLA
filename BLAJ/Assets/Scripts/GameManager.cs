4ddbba57a9ee640b914322b47fb92c29cee075ff
BrianConnors124 <bmconno000@birdvilleschools.net>
2024-09-27 14:35:35 -0500

    9/27/24 Updates

--- BLAJ/Assets/Scripts/GameManager.cs
+++ BLAJ/Assets/Scripts/GameManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -7,9 +8,20 @@ public class GameManager : MonoBehaviour
     public static GameManager instance;
 
     public int numberOfRooms;
-    public Vector2 previousDirection;
+    private bool collapser = true;
 
+    
+
+    [Header("Random Level Gen")]
+    [SerializeField] private Transform thisTransform;
+    [SerializeField] private Vector3 _location;
+    [SerializeField] private Vector2 _direction;
+    [SerializeField] private GameObject[] platform;
+    
     public int maxRooms;
+    public Vector2 previousDirection;
+    private BoxCollider2D thisCol;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -21,4 +33,67 @@ public class GameManager : MonoBehaviour
     {
         numberOfRooms++;
     }
+
+    public void Failed(Collider2D other)
+    {
+        
+        var ogGO = other.gameObject;
+        var secGO = thisCol.gameObject;
+        Destroy(secGO);
+        Debug.Log(ogGO.name + " is intersecting " + secGO.name);
+    }
+
+    public void SetValues(Vector2 a, GameObject[] b, BoxCollider2D c, Transform d)
+    {
+            _direction = a;
+            platform = b;
+            thisCol = c;
+            thisTransform = d;
+    }
+
+    public void InstantiateObj()
+    {
+        _location = thisTransform.position;
+        if (numberOfRooms < maxRooms)
+        { 
+            var randObj = Random.Range(0, platform.Length);
+            
+            
+            while (_direction.x == 0 && _direction.y == 0 && previousDirection.x == _direction.x || previousDirection.y == _direction.y)
+            {
+           
+                var rand = Random.Range(0, 2);
+
+                if (rand == 0)
+                {
+                    var x = Random.Range(-1, 2); 
+                    _direction = new Vector2(x, 0); 
+                    //Debug.Log("rand = 0");   
+                }
+
+                if (rand == 1)
+                {
+                    var y = Random.Range(-1, 1);
+                    _direction = new Vector2(0, y); 
+                    //Debug.Log("rand = 1");
+                }
+            }
+
+            previousDirection = new Vector2(_direction.x * -1, _direction.y * -1);
+            
+            BoxCollider2D platformCol = platform[randObj].GetComponent<BoxCollider2D>();
+            var thisScale = thisTransform.localScale;
+            var thisColSize = thisCol.size;
+            var nextColSize = platformCol.size;
+            var scaleSize = platform[randObj].transform.localScale;
+            if (collapser)
+            {
+                Instantiate(platform[randObj], new Vector2 ((_location.x + _direction.x * (nextColSize.x + 0.01f) * scaleSize.x / 2) + ((thisColSize.x + 0.01f) * thisScale.x * _direction.x /2),(_location.y + _direction.y * (nextColSize.y + 0.01f) * scaleSize.y / 2) + ((thisColSize.y + 0.01f) * thisScale.y * _direction.y /2)), Quaternion.identity);
+            }//instantiating obj
+            AddRoom();
+            // yield return new WaitWhile();
+        }
+        
+    }
+    
 }
--- BLAJ/Assets/Scripts/MapRandomizer.cs
+++ BLAJ/Assets/Scripts/MapRandomizer.cs
@@ -1,72 +1,49 @@
 
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class MapRandomizer : MonoBehaviour
 {
     [Header("Location And Direction")] 
-    [SerializeField] private Vector3 _location;
     [SerializeField] private Vector2 _direction;
 
     [Header("GameObject Inst")] 
     [SerializeField] private GameObject[] platform;
-
-    private bool a = true;
-
+    private Action redoInst;
+    private Action _instantiate;
     private BoxCollider2D thisCol;
+    private Transform thisTransform;
+    private bool canPlace = false;
+    private UniversalTimer timer = new UniversalTimer();
+    private bool placed = false;
     
-
-
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!timer.TimerDone)
+        {
+            GameManager.instance.Failed(other);
+        }
+    }
+    
     private void Start()
     {
-        StartCoroutine(InstantiateObj());
+        thisCol = GetComponent<BoxCollider2D>(); 
+        _direction = new Vector2(0, 0);
+        thisTransform = transform;
+        redoInst += ActionInstantiate;
+
+        StartCoroutine(timer.Timer(.1f));
+        StartCoroutine(new UniversalTimer().Timer(1, redoInst));
+        
     }
 
-    private IEnumerator InstantiateObj()
+    public void ActionInstantiate()
     {
-        yield return new WaitForSeconds(0.5f);
-        if (GameManager.instance.numberOfRooms < GameManager.instance.maxRooms)
-        { 
-            thisCol = GetComponent<BoxCollider2D>(); 
-            _direction = new Vector2(0, 0);
-            var rand = Random.Range(0, platform.Length);
-            _location = transform.position;
-            RandomXorY();
-            BoxCollider2D platformCol = platform[rand].GetComponent<BoxCollider2D>();
-            var thisScale = transform.localScale;
-            var thisColSize = thisCol.size;
-            var nextColSize = platformCol.size;
-            var scaleSize = platform[rand].transform.localScale;
-            if (a)
-            {
-                Instantiate(platform[rand], new Vector2 ((_location.x + _direction.x * nextColSize.x * scaleSize.x / 2) + (thisColSize.x * thisScale.x * _direction.x /2),(_location.y + _direction.y * nextColSize.y * scaleSize.y / 2) + (thisColSize.y * thisScale.y * _direction.y /2)), Quaternion.identity);
-            }//instantiating obj
-            GameManager.instance.AddRoom();
-        }
-    }
-
-    private void RandomXorY()
-    {
-        while (_direction.x == 0 && _direction.y == 0 && GameManager.instance.previousDirection.x == _direction.x || GameManager.instance.previousDirection.y == _direction.y)
-        {
-           
-            var rand = Random.Range(0, 2);
-
-            if (rand == 0)
-            {
-                var x = Random.Range(-1, 2); 
-                _direction = new Vector2(x, 0); 
-                Debug.Log("rand = 0");   
-            }
-
-            if (rand == 1)
-            {
-                var y = Random.Range(-1, 2);
-                _direction = new Vector2(0, y); 
-                Debug.Log("rand = 1");
-            }
-        }
-        GameManager.instance.previousDirection = new Vector2(_direction.x * -1, _direction.y * -1);
+        GameManager.instance.SetValues(_direction, platform, thisCol, thisTransform);
+        _instantiate += GameManager.instance.InstantiateObj;
+        _instantiate.Invoke();
     }
 }
