a817aa8b849545a403d8d0207a00884c40671f09
BrianConnors124 <bmconno000@birdvilleschools.net>
2024-11-01 14:11:38 -0500

    Idk why it broke, but i fixed it

--- BLAJ/Assets/Scripts/EnemyController.cs
+++ BLAJ/Assets/Scripts/EnemyController.cs
@@ -52,17 +52,19 @@ public class EnemyController : MonoBehaviour
     
     
     //return to beginning position ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    
+
     private void FixedUpdate()
     {
-        jumpLimit = new Vector2(transform.position.x, transform.position.y + (float) maxJumpHeight );//jump height limit
-        obstacleSensor = new Vector2(transform.position.x, transform.position.y);//testing to see if there is a collider in the direction of the enemy's movement
-        
-        
-        var limitTest = Physics2D.Raycast(jumpLimit,Vector2.right, lengthOfRay, LayerMask.GetMask("WorldObj"));
-        var sensor = Physics2D.Raycast(obstacleSensor,Vector2.right, lengthOfRay, LayerMask.GetMask("WorldObj"));
-        
-        
+        jumpLimit = new Vector2(transform.position.x, transform.position.y + (float)maxJumpHeight); //jump height limit
+        obstacleSensor =
+            new Vector2(transform.position.x,
+                transform.position.y); //testing to see if there is a collider in the direction of the enemy's movement
+
+
+        var limitTest = Physics2D.Raycast(jumpLimit, Vector2.right, lengthOfRay, LayerMask.GetMask("WorldObj"));
+        var sensor = Physics2D.Raycast(obstacleSensor, Vector2.right, lengthOfRay, LayerMask.GetMask("WorldObj"));
+
+
         if (!limitTest && sensor && !similarX && isTouchingGround())
         {
             Jump();
@@ -77,36 +79,53 @@ public class EnemyController : MonoBehaviour
         {
             pathBlocked = false;
         }
+
+        if (_player != null)
+        {
+
         var distance = new Vector2(_player.transform.localPosition.x - transform.position.x, transform.position.y); /*  this is finding the distance that the enemy should take to reach the player     EX: x2 - x1 = D
-                                                                                                            while keeping the same Y value and so the difference of the two is the distance the enemy must travel    */
+                                                                                                                        while keeping the same Y value and so the difference of the two is the distance the enemy must travel    */
         jumped = !isTouchingGround();
         if (cautious)
         {
-            var playerDist = (float)Math.Sqrt((Math.Pow(_player.transform.localPosition.x - transform.position.x, 2) + Math.Pow(_player.transform.localPosition.y - transform.position.y, 2)));
-            hostile = !Physics2D.Raycast(transform.position,  (_player.transform.localPosition - transform.localPosition).normalized , playerDist, LayerMask.GetMask("WorldObj"));
+            var playerDist = (float)Math.Sqrt((Math.Pow(_player.transform.localPosition.x - transform.position.x, 2) +
+                                               Math.Pow(_player.transform.localPosition.y - transform.position.y, 2)));
+            hostile = !Physics2D.Raycast(transform.position,
+                (_player.transform.localPosition - transform.localPosition).normalized, playerDist,
+                LayerMask.GetMask("WorldObj"));
             passive = false;
             GetComponent<SpriteRenderer>().color = Color.yellow;
         }
-        
+
         if (hostile)
         {
+            if (transform.position.x > _player.transform.position.x)
+            {
+                EnemyFaceRight(false);
+            }
+            else
+            {
+                EnemyFaceRight(true);
+            }
             // cautious = false;
             GetComponent<SpriteRenderer>().color = Color.red;
             Debug.DrawLine(transform.position, _player.transform.localPosition, Color.green, .1f);
-            if (distance.x >= distanceToStopBeforePlayer)//if this is true the enemy must move right
-            { 
+            if (distance.x >= distanceToStopBeforePlayer) //if this is true the enemy must move right
+            {
                 //the enemy should moving right
-                if (!pathBlocked && !jumped) 
-                { 
+                if (!pathBlocked && !jumped)
+                {
                     rb.velocity = new Vector2(npcMovementSpeed * Time.timeScale, rb.velocity.y); //applying the velocity
                     similarX = false;
                 }
-            } else if (distance.x <= -1 * distanceToStopBeforePlayer)//however if this is true the enemy must move left
-            { 
+            }
+            else if (distance.x <= -1 * distanceToStopBeforePlayer) //however if this is true the enemy must move left
+            {
                 //the enemy should moving left
                 if (!pathBlocked && !jumped)
-                { 
-                    rb.velocity = new Vector2(npcMovementSpeed * Time.timeScale * -1, rb.velocity.y); //applying the velocity
+                {
+                    rb.velocity =
+                        new Vector2(npcMovementSpeed * Time.timeScale * -1, rb.velocity.y); //applying the velocity
                     similarX = false; /*similarX is used to see if the player and enemy have an "distance" value of:  -1.08 < x < 1.08 , if so then this will be set to true,
                                               but in this case it isn't true so it is set to false. This is to prohibit the enemy from continuously walking into the player when it is already close enough. */
                 }
@@ -114,27 +133,18 @@ public class EnemyController : MonoBehaviour
             else
             {
                 if (!pathBlocked && !jumped)
-                { 
+                {
                     similarX = true; //as previously stated this is set to true because "distance" is not greater than 1.08 nor is it less than -1.08 meaning it fits the inequality of -1.08 < x < 1.08 so it shouldn't move anymore
-                    rb.velocity = new Vector2(0, rb.velocity.y);//preventing anymore movement along the X axis
+                    rb.velocity = new Vector2(0, rb.velocity.y); //preventing anymore movement along the X axis
                     //similarX is used to prevent the enemy from jumping when under the player, however I didn't set the Y velocity to zero because if the enemy is falling I want it to fall onto solid ground instead of staying in the air
                 }
             }
         }
         else
         {
-            if(_player != null)
-                Debug.DrawLine(transform.localPosition, _player.transform.localPosition, Color.red, .1f);
+            Debug.DrawLine(transform.localPosition, _player.transform.localPosition, Color.red, .1f);
         }
 
-        if (transform.position.x > _player.transform.position.x)
-        {
-            EnemyFaceRight(false);
-        }
-        else
-        {
-            EnemyFaceRight(true);
-        }
         if (passive)
         {
             if (!jumped)
@@ -143,9 +153,10 @@ public class EnemyController : MonoBehaviour
                 passive = false;
             }
         }
-        
-        
     }
+
+
+}
     
     private void OnTriggerExit2D(Collider2D other)
     {
