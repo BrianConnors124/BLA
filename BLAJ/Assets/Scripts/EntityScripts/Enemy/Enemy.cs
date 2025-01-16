e856349d22a8d6ea0777164b200676409c1699e3
BrianConnors124 <148557781+BrianConnors124@users.noreply.github.com>
2025-01-16 15:13:37 -0600

    bruh

--- BLAJ/Assets/Scripts/EntityScripts/Enemy/Enemy.cs
+++ BLAJ/Assets/Scripts/EntityScripts/Enemy/Enemy.cs
@@ -163,13 +163,13 @@ public class EnemyInfo : ScriptableObject
     public string named;
     public AnimatorController anim;
     public string description;
-    public float health;
-    public float jumpHeight;
-    public float movementSpeed;
-    public float damage;
-    public float baseReach;
-    public float stun;
-    public float knockBack;
-    public float primaryCD;
+    public float health = 100;
+    public float jumpHeight = 12;
+    public float movementSpeed = 6;
+    public float damage = 10;
+    public float baseReach = 1;
+    public float stun = 0;
+    public float knockBack = 10;
+    public float primaryCD = 1;
     public bool bossType;
 }
